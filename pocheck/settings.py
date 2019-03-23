5ec2a66cf57175ec0ced2f3af8dd350ed3bdeecd
Kwansu <bicsu@naver.com>
2019-03-23 13:21:46 +0000

    add allowd url

--- pocheck/settings.py
+++ pocheck/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'y1w=o8_rv*d2jrp8h1^eg2w0b0#ui6*av4^2!g%ml*-_0=ew)&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pocheck-bicsu.c9users.io']
+ALLOWED_HOSTS = ['pocheck-bicsu.c9users.io',
+'localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
