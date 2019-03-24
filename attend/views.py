9379e4570c08c6c9d28e865cd3763b5d8e02a255
Kwansu <bicsu@naver.com>
2019-03-24 13:04:38 +0000

    edit view

--- attend/views.py
+++ attend/views.py
@@ -56,37 +56,15 @@ def keyboard(request):
         'buttons':['오늘','내일']
     })
  
-# @csrf_exempt
-# def answer(request):
- 
-#     json_str = ((request.body).decode('utf-8'))
-#     received_json_data = json.loads(json_str)
-#     datacontent = received_json_data['content']
- 
-#     if datacontent == '오늘':
-#         today = "오늘 급식"
- 
-#         return JsonResponse({
-#                 'message': {
-#                     'text': today
-#                 },
-#                 'keyboard': {
-#                     'type':'buttons',
-#                     'buttons':['오늘','내일']
-#                 }
- 
-#             })
- 
-#     elif datacontent == '내일':
-#         tomorrow = "내일 급식"
- 
-#         return JsonResponse({
-#                 'message': {
-#                     'text': tomorrow
-#                 },
-#                 'keyboard': {
-#                     'type':'buttons',
-#                     'buttons':['오늘','내일']
-#                 }
- 
-#             })
+@csrf_exempt
+def message(request):
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    content = return_json_str['content']
+
+    if content == '사용법':
+        return JsonResponse({
+            'message': {
+                'text': 'KIN',
+            },
+        })
