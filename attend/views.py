97b8a1cbc6053d9323fb2db18b4ec6fb06ba77e3
Kwansu <bicsu@naver.com>
2019-03-27 06:31:32 +0000

    add crawling

--- attend/views.py
+++ attend/views.py
@@ -5,6 +5,13 @@ from .forms import PostForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
+import dateimte
+from bs4 import BeautifulSoup
+import random
+import os
+import requests
+import re
+
 
 # Create your views here.
 # def post_list(request):
@@ -57,7 +64,7 @@ def keyboard(request):
         'type':'buttons',
         'buttons':['출석체크 확인', '오늘 RIST 식단','시간표']
     })
- 
+
 @csrf_exempt
 def message(request):
     img_bool = False
@@ -66,11 +73,50 @@ def message(request):
     datacontent = received_json_data['content']
     
     if datacontent =='출석체크 확인':
-        ima_bool = False
         msg = '이 기능은 아직;;'
     elif datacontent == '오늘 RIST 식단':
-        ima_bool = False
-        msg = "https://ssgfoodingplus.com/fmn101.do?goTo=todayMenu&storeCd=05600"
+        url = 'https://ssgfoodingplus.com/fmn101.do?goTo=todayMenuJson'
+        yearmonth = datetime.datetime.now().strftime("%Y-%m-")
+        now = datetime.datetime.now()
+        month = now.month
+        day = now.day
+        r = datetime.datetime.today().weekday()
+        hour = datetime.datetime.now().hour
+        if hour>=14:
+            if r==6:
+                r=0
+            else :
+                r+=1
+            if month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
+                if day==31:
+                    month+=1
+                    day=1
+                else:
+                    day+=1
+            else:
+                if day==30:
+                    month+=1
+                    day=1
+                else :
+                    day+=1
+        
+        today = yearmonth+str("%02d"%day)
+        days=["월","화","수","목","금","토","일"]
+        payloads = {"storeCd": "05600", "cafeCd": "01", "menuDate": today}
+        res = requests.post(url, data= payloads).json()
+        breakfast=""
+        lunch=""
+        dinner=""
+        for i in range(0,len(res['result'])):
+            if res['result'][i]['meal_type_nm']=="조식":
+                breakfast+=res['result'][i]['if_menu_nm']+"\n"
+            elif res['result'][i]['meal_type_nm']=="중식":
+                lunch+=res['result'][i]['if_menu_nm']+"\n"
+            elif res['result'][i]['meal_type_nm']=="석식":
+                dinner+=res['result'][i]['if_menu_nm']+"\n"
+        return_msg ="RIST식당/{0}요일\n-------조식-------\n{1}\n-------중식-------\n{2}\n-------석식-------\n{3}\n".format(days[r],breakfast,lunch,dinner)
+        
+        
     elif datacontent =='시간표':
         img_bool = True
         msg = 'B반의 시간표'
